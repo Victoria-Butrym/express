1608096265d8702801eff673fcd25c31cbb8cb97
Victoria Butrym <victoria-butrym@tut.by>
2022-02-05 16:24:41 +0300

    1.0 [express] created basic endpoints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 const express = require('express');
+const path = require('path')
+const data = require('./data/data.json');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`server is listening on port ${PORT}`))
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+app.get('/', (req, res) => {
+    res.send(data)
+})
+
+app.post('/post', (req, res) => {
+    res.send(`post request on /post route on port ${PORT}`)
+})
+
+app.put('/put', (req, res) => {
+    res.send(`put request on /put route on port ${PORT}`)
+})
+
+app.delete('/delete', (req, res) => {
+    res.send(`delete request on /delete route on port ${PORT}`)
+})
+
+app.listen(PORT, () => {
+    console.log(`server is listening on port ${PORT}`);
+})
